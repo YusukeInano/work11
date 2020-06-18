06cc31d11accf7f35c77302477384469317aa08a
Yusuke Inano <nagare5296@yahoo.co.jp>
2020-06-18 06:06:32 +0000

    practice commit

---
+++ practice.php
@@ -0,0 +1,33 @@
+<?php
+echo 'hello php!';
+$a = 3;
+$b = 7;
+$new_integer = $a + $b;
+echo $new_integer;
+$array = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月",];
+echo $array[7];
+$hello = "Hello,";
+$name = "Yusuke_Inano";
+$world = "'s_World";
+echo $hello;
+echo $name;
+echo $world;
+$tech_boost = "tech";
+echo $tech_boost;
+$tech_boost .= " boost";
+echo $tech_boost;
+$_2018_calender = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+echo $_2018_calender["December"];
